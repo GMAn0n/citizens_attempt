9d0eca839e697c8252b5616e4aff92dba4c2507d
GMAn0n <75751135+GMAn0n@users.noreply.github.com>
2024-01-30 00:43:35 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ const button02 = nftMetadata?.raw?.city;
 
 
 const frameMetadata = getFrameMetadata({
-  buttons: ['randomize nft','button02'],
+  buttons: ['randomize nft','${button02}'],
   image: nftImageUrl,
   post_url: postUrl,
 });
