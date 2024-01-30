ece89d6d844e5a8a0f576d6a1c9ccb3818a456c4
GMAn0n <75751135+GMAn0n@users.noreply.github.com>
2024-01-29 22:49:09 -0500

    Update getNftMetadata.ts

--- app/lib/getNftMetadata.ts
+++ app/lib/getNftMetadata.ts
@@ -1,7 +1,7 @@
 export const getNftMetadata = async (nft: string, tokenId: number) => {
   const options = {method: 'GET', headers: {accept: 'application/json'}};
   const Url = 'https://eth-mainnet.g.alchemy.com/';
-  const endpoint = Url + `nft/v3/${process.env.ALCHEMY_ID || 'docs-demo'}/getNFTMetadata`;
+  const endpoint = Url + `nft/v2/${process.env.ALCHEMY_ID || 'docs-demo'}/getNFTMetadata`;
   const params = `?contractAddress=${nft}&tokenId=${tokenId}&refreshCache=false`;
   const response = await fetch(endpoint+params, options);
   return await response.json();
